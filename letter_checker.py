7047ad94ba407a65a8160df8ae11b0ffadbb9508
kaden530 <bro22001@byui.edu>
2022-05-19 07:23:55 -0600

    Classes for the program

---
+++ letter_checker.py
@@ -0,0 +1,13 @@
+class Letters:
+    def __init__(self):
+        self.letters = []
+
+    def letter_ckeck(self):
+        if self.letters.lower() in self.guess.lower():
+            print("Please guess a new letter you've already guessed that letter.")
+            _guess = 1
+            return _guess
+        else:
+            self.letters.append(self.guess)
+            _guess = 2
+            return _guess
