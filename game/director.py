ca6d0d251f60bf095700668508d6a64112d63654
Nikita <iamspecial19@gmail.com>
2022-05-24 12:34:34 +0800

    Pull in attribute from terminal_service.

--- game/director.py
+++ game/director.py
@@ -1,14 +1,13 @@
 from game.word_bank import Words
 from game.letter_checker import Letters
-from game.terminalService import terminalService
-import random
+from game.terminal_service import terminalService
 
 class Director:
     def __init__(self):
-
-        self._word_bank = Words
-        # self._letter_checker = Letters
-        self._terminalService = terminalService
+        self._word_bank = Words()
+        self._letter_checker = Letters()
+        self._terminalService = terminalService()
+        self.word = "polly"
         self._is_playing = True
 
     def start_game(self):
@@ -18,43 +17,26 @@ class Director:
             # self._do_outputs()
     
     def _get_inputs(self):
-        while self._is_playing:
-            self.user_input = input('Guess a letter [a-z]: ')
-            return self.user_input
-        
+        """
+        Ask the user to guess a letter.
+        """
+        # while self._is_playing:
+        self._terminalService.letter = input('Guess a letter [a-z]: ')
+        self._letter_checker.letters.append(self._terminalService.letter)
+        # return self.user_input
         
     def _do_updates(self):
-        words = ['january', 'february', 'march']
-        self.random_word = random.choice(words)
-
-        wrong_letter_count = 0
-
+        self._terminalService.word = self._word_bank.random_word()
+        self._terminalService.print_board()
+    
         for self.random_word in self.random_word:
             if self.user_input in self.random_word:
                 print(self.user_input, end=' ')
             else:
                 print('_', end=' ')
-                wrong_letter_count = wrong_letter_count + 1 # If guess wrong, increase failure count by 1.
         print('')
     
-        # failure_count = 6
-        
-
-        # # As long as the failure count is less than zero the game will keep going. Else, the game can continue.
-        # while failure_count > 0:
-        #     if wrong_letter_count == 0:
-        #         print('You won.')
-        #         break # If failure count still remain 0, break out from the loop.
-
-
-        # if self.user_input in self.random_word:
-        #     print('Correct guess.')
-        # else:
-        #     print(f'Incorrect. {failure_count} turn left.')
-        #     failure_count = failure_count - 1 # If guess correct, decrease failure count by 1.
-
-        # Whole collection of letter that has been guessed.
-        # letter_guessed = letter_guessed + self.user_input
+    
         
         
 
